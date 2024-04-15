b3adc782da5295a1f136ca1c0a63797aa0bc239a
HallisCode <94699197+HallisCode@users.noreply.github.com>
2024-04-15 21:45:45 +0300

    Database. Реализовал все репозитории на основе интерфейсов IQUERY, каждый такой интерфейс представляет определенный метод. Произвел ренейм папок и namespaces. (#10)

---
+++ src/Core/Database/IQuery/IQueryUpdate.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Database.IQuery
+{
+	public interface IQueryUpdate<TEntity>
+	{
+		Task UpdateAsync(TEntity entity);
+	}
+}
---
+++ src/Core/Database/IRepositories/Classification/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Database.IQuery.Select;
+using Domain.Entities.Classification;
+
+namespace Database.IRepositories.Classification
+{
+	public interface ICategoryRepository : IBaseOperations<Category>, IQuerySelectAllLike<Category>
+	{
+
+	}
+}
