ffce4d5095c2a0052f4cc3db68c2872fac0f4458
HallisCode <applgoolhal@gmail.com>
2024-04-20 13:25:04 +0300

    Application. Разделил сервисы на 2 папки IServices && Services. Инициализировал сервисы IBookService, IUserService

---
+++ src/Core/Application/IServices/Book_/IBookService.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTO.Requests.Book_;
+using Application.DTO.Responses.Book_;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.IServices.Book_
+{
+	public interface IBookService
+	{
+		Task<List<BookDTO>> FindBooksAsync(FindBooksDTO findBooksDTO);
+
+		Task<BookDTO> GetBookAsync(ulong ID);
+
+		Task<BookDTO> CreateBookAsync(CreateBookDTO createBookDTO);
+
+		Task UpdateBookAsync(UpdateBookDTO updateBookDTO);
+
+		Task DeleteBookAsync(ulong bookID);
+
+	}
+}
---
+++ src/Core/Application/Services/Book_/BookService.cs
@@ -0,0 +1,37 @@
+﻿using Application.DTO.Requests.Book_;
+using Application.DTO.Responses.Book_;
+using Application.IServices.Book_;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Services.Book_
+{
+	internal class BookService : IBookService
+	{
+		public Task<BookDTO> CreateBookAsync(CreateBookDTO createBookDTO)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteBookAsync(ulong bookID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<BookDTO>> FindBooksAsync(FindBooksDTO findBooksDTO)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<BookDTO> GetBookAsync(ulong ID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateBookAsync(UpdateBookDTO updateBookDTO)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
