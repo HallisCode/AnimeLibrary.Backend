fce9975f0dc0a490e5aca1f41e3de669c0aacc6e
HallisCode <applgoolhal@gmail.com>
2024-04-21 15:45:01 +0300

    Domain. Создал enum и структуру для BookRating

---
+++ src/Core/Domain/ValueObjects/Rating.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Domain.ValueObjects
+{
+	public record Rating(int Value) : IComparable<Rating>, IComparable
+	{
+
+		public int CompareTo(Rating other)
+		{
+			if (this.Value < other.Value) return -1;
+
+			if (this.Value == other.Value) return 0;
+
+			return 1;
+		}
+
+		public int CompareTo(object obj)
+		{
+			return -1;
+		}
+	}
+}
