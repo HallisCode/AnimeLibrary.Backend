b28a1f25d576855b30bbce010eab8e001ea7da44
HallisCode <applgoolhal@gmail.com>
2024-04-21 15:43:24 +0300

    Domain. Добавил пару новых валидаторов для доменных сущностей

--- src/Core/Domain/Validation/Models/UserValidator.cs
+++ src/Core/Domain/Validation/Models/UserValidator.cs
@@ -1,24 +1,17 @@
-﻿using Domain.IEntites.User_;
+﻿using Domain.Entities.User_;
 using Domain.Validation.Properties;
 using FluentValidation;
 
 namespace Domain.Validation.Models
 {
-	public class UserValidator : AbstractValidator<IUser>
+	public class UserValidator : AbstractValidator<User>
 	{
 		/// <summary>
 		/// Валидатор для модели <see cref="IUser"/>.
 		/// </summary>
-		/// <param name="isCreating">Флаг, указывающий создается ли новый пользователь. 
-		/// Если значение <b>true</b>, то поля не могут быть null или пустыми</param>
-		public UserValidator(bool isCreating = true)
+		public UserValidator()
 		{
-			IRuleBuilder<IUser, string> username = RuleFor(user => user.Username).Username();
-
-			if (isCreating)
-			{
-				username.NotEmpty();
-			}
+			RuleFor(user => user.Username).Username();
 		}
 	}
 }
