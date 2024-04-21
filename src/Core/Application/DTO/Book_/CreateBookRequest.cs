3c84342c1cbeef26917b0b9d9ded671785fe1848
HallisCode <applgoolhal@gmail.com>
2024-04-21 13:13:20 +0300

    Application. Добавил к DTO реализацию IEntitie

--- src/Core/Application/DTO/Book_/CreateBookRequest.cs
+++ src/Core/Application/DTO/Book_/CreateBookRequest.cs
@@ -1,6 +1,19 @@
-﻿namespace Application.DTO.Book_
+﻿using Domain.IEntites.Book_;
+
+namespace Application.DTO.Book_
 {
-	public record CreateBookRequest
+	public record CreateBookRequest : IBook
 	{
+		public string Title => throw new System.NotImplementedException();
+
+		public string ISBN13 => throw new System.NotImplementedException();
+
+		public int YearRelease => throw new System.NotImplementedException();
+
+		public int CountPages => throw new System.NotImplementedException();
+
+		public string Description => throw new System.NotImplementedException();
+
+		// Дополнительные поля которые могут понадобиться
 	}
 }
