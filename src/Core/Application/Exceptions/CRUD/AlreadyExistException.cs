1fd94a69c2cb81b915e33a8070c552120f5cb8c9
HallisCode <applgoolhal@gmail.com>
2024-04-22 19:53:58 +0300

    Application. Добавил Exceptions/CRUD/AdlreadyExistException

---
+++ src/Core/Application/Exceptions/CRUD/AlreadyExistException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Application.Exceptions.CRUD
+{
+	public class AlreadyExistException : ApplicationException
+	{
+		public AlreadyExistException(string message) : base(message)
+		{
+
+		}
+
+		public AlreadyExistException(string message, Exception innerException) : base(message, innerException)
+		{
+
+		}
+	}
+}
