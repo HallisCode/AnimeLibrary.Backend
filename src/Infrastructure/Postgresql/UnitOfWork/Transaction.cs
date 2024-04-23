1cfaea3c3bb6c4b58f7e18addde7776078ab6ed3
HallisCode <applgoolhal@gmail.com>
2024-04-23 21:15:27 +0300

    Postgresql. Начал реализацию ITransaction

---
+++ src/Infrastructure/Postgresql/UnitOfWork/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using Database.IUnitOfWork;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Postgresql.UnitOfWork
+{
+	public class Transaction : ITransaction
+	{
+		protected IDbContextTransaction transaction;
+
+		public Transaction(IDbContextTransaction transaction)
+		{
+			this.transaction = transaction;
+		}
+
+		public Task CommitAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task RollBackAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SaveChangesAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
