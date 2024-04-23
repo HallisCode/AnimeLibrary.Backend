69cd6bfbfd2985d5f3a56ac35159c16b355360b7
HallisCode <applgoolhal@gmail.com>
2024-04-23 22:13:48 +0300

    Pogstresql. Незначительные правки.

---
+++ src/Infrastructure/Postgresql/Repositories/Auth/SecurityRepository.cs
@@ -0,0 +1,44 @@
+﻿using Database.IRepositories.Auth;
+using Domain.Entities.Auth;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace Postgresql.Repositories.Auth
+{
+	public class SecurityRepository : ISecurityRepository
+	{
+		protected ApplicationDbContext dbContext;
+
+		public async Task CreateAsync(Security entity)
+		{
+			dbContext.Security.Add(entity);
+
+
+			await dbContext.SaveChangesAsync();
+		}
+
+		public async Task DeleteFirstByAsync(Security entity)
+		{
+			dbContext.Remove(entity);
+
+
+			await dbContext.SaveChangesAsync();
+		}
+
+		public async Task<Security> GetFirstByAsync(Expression<Func<Security, bool>> predicate)
+		{
+			return await dbContext.Security.FirstOrDefaultAsync(predicate);
+		}
+
+		public async Task UpdateAsync(Security entity)
+		{
+			dbContext.Security.Update(entity);
+
+
+			await dbContext.SaveChangesAsync();
+		}
+	}
+}
