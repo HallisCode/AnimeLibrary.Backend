e792a4f670f5114e0501f54b3aa41fb0e5b81923
HallisCode <applgoolhal@gmail.com>
2024-04-30 16:47:50 +0300

    GraphQL. Добавил контроллер регистрации.

---
+++ src/Presentation/GraphQL/Controllers/Registry/RegistryController.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTO.Registry;
+using Application.IServices;
+using GraphQL.AspNet.Attributes;
+using GraphQL.AspNet.Controllers;
+using GraphQL.AspNet.Interfaces.Controllers;
+using GraphQL.DTO.Registry;
+using GraphQL.Mapping;
+using System.Threading.Tasks;
+
+namespace GraphQL.Controllers.Registry
+{
+	public class RegistryController : GraphController
+	{
+		private IRegistryService registryService;
+
+
+		public RegistryController(IRegistryService registryService)
+		{
+			this.registryService = registryService;
+		}
+
+
+		[MutationRoot("registry", typeof(bool))]
+		public async Task<IGraphActionResult> Registry(DTO.Registry.RegistryRequest request)
+		{
+			await registryService.RegistryAsync(request.ToApplicationDTO());
+
+			return this.Ok(true);
+		}
+	}
+}
--- src/Presentation/GraphQL/Program.cs
+++ src/Presentation/GraphQL/Program.cs
@@ -1,3 +1,14 @@
+using Application.IServices;
+using Application.Services;
+using Database.IUnitOfWork;
+using GraphQL.AspNet.Configuration;
+using GraphQL.VariableConfugiration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Postgresql;
+using Postgresql.UnitOfWork;
+
 namespace GraphQL
 {
 	public class Program
@@ -5,9 +16,41 @@ namespace GraphQL
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+
+			//________________________
+			// Конфигурация переменных
+			//________________________
+
+			string postgresql = builder.GetPostgrsqlConnectionString();
+
+			//_____________________________
+			// Подключение сервисов
+			//_____________________________ 
+
+			// GraphQl
+			var schemaBuilder = builder.Services.AddGraphQL();
+
+			// Сервисы логики
+			builder.Services.AddScoped<IRegistryService, RegistryService>();
+
+			// Репозитории и доступ к бд
+			builder.Services.AddDbContext<ApplicationDbContext>(
+				options => options.UseNpgsql(postgresql), ServiceLifetime.Transient
+			);
+			builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+			//_________________________________
+			// Middlewares свойственные GraphQL
+			//_________________________________
+
 			var app = builder.Build();
 
-			app.MapGet("/", () => "Hello World!");
+			//________________________________
+			// Подключение middlewares
+			//________________________________
+
+			// GraphQl
+			app.UseGraphQL();
 
 			app.Run();
 		}
