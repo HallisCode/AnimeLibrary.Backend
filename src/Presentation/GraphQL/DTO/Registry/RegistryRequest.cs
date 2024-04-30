24409b149d1bd6e0972726bc439e20200ce64682
HallisCode <applgoolhal@gmail.com>
2024-04-30 16:47:19 +0300

    GraphQL. Добавил DTO && Mapping со слоем application

---
+++ src/Presentation/GraphQL/DTO/Registry/RegistryRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GraphQL.DTO.Registry
+{
+	public class RegistryRequest
+	{
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Username { get; set; }
+		[Required]
+		public string Password { get; set; }
+
+
+		public RegistryRequest() { }
+	}
+}
---
+++ src/Presentation/GraphQL/Mapping/RegistryMapping.cs
@@ -0,0 +1,15 @@
+﻿
+namespace GraphQL.Mapping
+{
+	public static class RegistryMapping
+	{
+		public static Application.DTO.Registry.RegistryRequest ToApplicationDTO(this DTO.Registry.RegistryRequest source)
+		{
+			return new Application.DTO.Registry.RegistryRequest(
+				Email: source.Email,
+				Password: source.Password,
+				Username: source.Username
+				);
+		}
+	}
+}
